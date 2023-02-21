aca2a5301dd9bd97f9cbf9d53d542e66f6ad6a5e
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-20 23:50:28 -0300

    chore: restart project

--- back/src/customer/useCases/createCustomer.ts
+++
@@ -1,8 +0,0 @@
-import { Customer } from '../entity'
-import { CustomerRepository } from '../repository'
-
-export const CreateCustomerUseCase = async (customer: Customer) => {
-  const customerRepository: CustomerRepository = new CustomerRepository()
-
-  await customerRepository.create(customer)
-}
--- back/src/shared/entity.ts
+++
@@ -1,6 +0,0 @@
-export abstract class Entity<T> {
-  public readonly props: T
-  constructor(props: T) {
-    this.props = props
-  }
-}
